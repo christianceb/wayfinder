0ace4b549685e3c43f25e59556c18c11b6d1cf32
Christian Ponce <12862119+christianceb@users.noreply.github.com>
2020-10-10 17:31:43 +0800

    Show user location on map and implemented functionality to center map on user

---
+++ components/maps/Baseline.js
@@ -0,0 +1,4 @@
+const Lat = -31.9529;
+const Lng = 115.8605;
+
+export default {Lat, Lng};
--- components/maps/Main.js
+++ components/maps/Main.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import MapboxGL, { MapView, Camera } from "@react-native-mapbox-gl/maps";
-
-MapboxGL.setAccessToken("pk.eyJ1IjoiY2hyaXN0aWFuY2ViIiwiYSI6ImNrZDN4MzQyODEwcTMyc251ZGJnY3R2aDYifQ.Iip2TLYYP-vYS145IdHWXQ")
+import { View } from 'react-native';
+import MapboxGL, { MapView, Camera, UserLocation } from "@react-native-mapbox-gl/maps";
+import { FAB } from 'react-native-paper';
+import Baseline from './Baseline'
 
 export class Main extends Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            timestamp: 0,
+            latitude: Baseline.Lat,
+            longitude: Baseline.Lng,
+        };
+
+        this.onUserLocationUpdate = this.onUserLocationUpdate.bind(this);
+        this.onRegionDidChange = this.onRegionDidChange.bind(this);
+        this.centerPosition = this.centerPosition.bind(this);
+    }
+
+    centerPosition() {
+        this._camera.setCamera({
+            centerCoordinate: [this.state.longitude, this.state.latitude],
+            zoomLevel: 17,
+            animationDuration: 2000,
+        });
+    }
+
+    async onRegionDidChange() {
+        const visibleBounds = await this._map.getVisibleBounds();
+        console.log(visibleBounds)
+    }
+
+    onUserLocationUpdate(location) {
+        this.setState({
+            timestamp: location.timestamp,
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+        });
     }
 
     componentDidMount() {
+        MapboxGL.setAccessToken("pk.eyJ1IjoiY2hyaXN0aWFuY2ViIiwiYSI6ImNrZDN4MzQyODEwcTMyc251ZGJnY3R2aDYifQ.Iip2TLYYP-vYS145IdHWXQ")
         MapboxGL.setTelemetryEnabled(false)
     }
 
     render() {
         return (
             <View style={{flex: 1}}>
-                <Text>Well</Text>
-                <MapView style={{flex: 1}}>
-                    <Camera centerCoordinate={[115.8605, -31.9529]} zoomLevel={10} />
+                <MapView style={{flex: 1}} ref={(map) => (this._map = map)} onRegionDidChange={this.onRegionDidChange}>
+                    <UserLocation visible={true} onUpdate={this.onUserLocationUpdate} />
+                    <Camera ref={(camera) => (this._camera = camera)} centerCoordinate={[Baseline.Lng, Baseline.Lat]} zoomLevel={10} />
                 </MapView>
+                <FAB icon="map-marker" large onPress={this.centerPosition} style={{position: 'absolute', right: 0, bottom: 0, margin: 16, backgroundColor: "#da272d"}} />
             </View>
         );
     }
