3229153ccb35ec2fbc386a9d0f7eeea5e437fcc8
Christian Ponce <12862119+christianceb@users.noreply.github.com>
2020-11-07 18:15:40 +0800

    Implemented chips in search filtering the results by type - Major optimisations to search

--- components/screens/Search.js
+++ components/screens/Search.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
   SafeAreaView, 
@@ -12,40 +12,78 @@ import {
 import { Card, Title, Paragraph, Chip } from 'react-native-paper';
 import { RadioButton } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import WF_Off from '~/Wayfinder_Offline.js';
 
 const Search = ( { navigation } ) => {
-  const [data, setData] = useState(global.locationsData);
+  const [data, setData] = useState([]);
   const [filterLocation, setFilterLocation] = useState(false);
-  const [chipValue, setChipValue] = useState('')
+  const [chipValue, _setChipValue] = useState(null)
+  const [queryString, _setQueryString] = useState("")
+  const TYPE_MAP = ["Campuses", "Buildings", "Rooms"];
 
-  const returnParents = (parent) => {
-    let parentLocation = global.locationsData.find(item => item.id == parent);
-    let parentsString = parentLocation.name;
+  const returnParents = (parent_id) => {
+    let parentLocation = WF_Off.findLocationById(parent_id)
+    let parentsString = parentLocation.name
 
-    if (parentLocation.parent_id !== null) {
+    if (parentLocation.parent_id) {
       parentsString += ", " + returnParents(parentLocation.parent_id)
     }
+
     return parentsString
   }
 
-  function filter(text) {
-    let locArray = global.locationsData    
-    const newLocation = locArray.filter(item => {      
-      const itemData = `${item.name.toLowerCase()}`;
-      
-      const textData = text.toLowerCase();
-        
-      return itemData.indexOf(textData) > -1;    
-    });
-    setData(newLocation)
-  };
+  const setQueryString = (value) => {
+    _setQueryString(value)
+    filter(value)
+  }
+
+  const filter = (text) => {
+    let dataset = []
+    text = toSearcheableString(text)
+
+    if (chipValue) {
+      dataset = WF_Off.getLocationsByType(chipValue)
+    } else {
+      dataset = WF_Off.getAllLocations(true);
+    }
+
+    if (text != "" && chipValue === null && text.length >= 3) {
+      dataset = dataset.filter(item => {
+        // Exit early if filter is set and item does not match filter
+        if (chipValue && item.type != chipValue) {
+          return false
+        }
+
+        const itemData = toSearcheableString(item.name)
+
+        return itemData.indexOf(text) > -1;
+      })
+    }
+
+    setData(dataset)
+  }
+
+  const toSearcheableString = (value) => {
+    return value
+      .toLowerCase()
+      .replace(/\W+/g, "")
+      .trim()
+  }
+
+  useEffect(() => {
+    if (!filterLocation) {
+      filter(queryString)
+    }
+  }, [chipValue, filterLocation]);
+
 
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={styles.searchContainer}>
         <TextInput
           style={styles.inputStyle}
-          onChangeText={value => filter(value)}
+          value={queryString}
+          onChangeText={value => { setQueryString(value) }}
           autoCorrect={false}
           />
         <Icon style={styles.icon} name='search' color='#000' size={25}/>
@@ -58,18 +96,18 @@ const Search = ( { navigation } ) => {
               <Text style={styles.head}>
                 Add a Filter
               </Text>
-              <RadioButton.Group onValueChange={value => setChipValue(value)} value={chipValue}>
+              <RadioButton.Group onValueChange={value => _setChipValue(value)} value={chipValue}>
                 <View style={styles.items}>
-                  <Text>Campuses</Text>
-                  <RadioButton value="Campuses" />
+                  <Text>{TYPE_MAP[0]}</Text>
+                  <RadioButton value="0" />
                 </View>
                 <View style={styles.items}>
-                  <Text>Buildings</Text>
-                  <RadioButton value="Buildings" />
+                  <Text>{TYPE_MAP[1]}</Text>
+                  <RadioButton value="1" />
                 </View>
                 <View style={styles.items}>
-                  <Text>Rooms</Text>
-                  <RadioButton value="Rooms" />
+                  <Text>{TYPE_MAP[2]}</Text>
+                  <RadioButton value="2" />
                 </View>
               </RadioButton.Group>
               <View style={styles.button}>
@@ -82,13 +120,16 @@ const Search = ( { navigation } ) => {
         <Icon style={styles.filterIcon} name='filter-list' color='black' size={30}
           onPress={() => setFilterLocation(true)}
         />
-        <Chip mode='outlined' style={styles.chip} onClose={() => console.log('closed')}>
-          {chipValue}
-        </Chip>
+        {chipValue &&
+          <Chip mode='outlined' style={styles.chip} onClose={() => _setChipValue(null)}>
+            {TYPE_MAP[chipValue]}
+          </Chip>
+        }
       </View>
       <FlatList
         data={data}
         keyExtractor={({ id }, index) => id.toString()}
+        initialNumToRender={10}
         renderItem={({ item }) => (
         <Card 
           style={styles.card} 
@@ -100,7 +141,7 @@ const Search = ( { navigation } ) => {
           >
           <Card.Content>
             <Title>{item.name}</Title>
-                {item.parent_id == null ? <Paragraph>{item.name}</Paragraph> : <Paragraph>{returnParents(item.parent_id)}</Paragraph>}
+            <Paragraph>{item.parent_id ? returnParents(item.parent_id) : item.name}</Paragraph>
           </Card.Content>
         </Card>
         )}
