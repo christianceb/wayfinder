6a31fe52ecb73497c1700f7d658faa422d35369f
Christian Ponce <12862119+christianceb@users.noreply.github.com>
2020-11-20 12:05:00 +0800

    Obsoleted old offline map data on site

--- index.js
+++ index.js
@@ -5,12 +5,9 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import LocationsData from '~/locations/data.json'
 import MapboxGL from "@react-native-mapbox-gl/maps";
 
 MapboxGL.setAccessToken("pk.eyJ1IjoiY2hyaXN0aWFuY2ViIiwiYSI6ImNrZDN4MzQyODEwcTMyc251ZGJnY3R2aDYifQ.Iip2TLYYP-vYS145IdHWXQ")
 MapboxGL.setTelemetryEnabled(false)
-
-global.locationsData = LocationsData
 
 AppRegistry.registerComponent(appName, () => App);
